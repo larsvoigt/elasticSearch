2edfc49216bae6fd80dd56c28e5565761062467c
Lars Voigt <lars.voigt@dzb.de>
2015-04-10 15:18:28 +0200

    - added  npm package

--- elasticTestClient.js
+++ elasticTestClient.js
@@ -7,22 +7,22 @@ var client = new elasticsearch.Client({
     host: 'localhost:9200'
 });
 
-//client.search({
-//    index: 'readium',
-//    body: ejs.Request().query(ejs.QueryStringQuery('"content by epub:trigger elements is supported. If this text is rendered, the audio element is not supported and the test fails"'))
-//}).then(function (resp) {
-//    var hits = resp.hits.hits;
-//    console.log(hits);
-//}, function (err) {
-//    console.trace(err.message);
-//});
-
-client.suggest({
+client.search({
     index: 'readium',
-    body: ejs.TermSuggester('mysuggester')
-        .text('epub')
-        .field('body')
-}, function (error, response) {
-    var hits = response.hits;
+    body: ejs.Request().query(ejs.QueryStringQuery('"content by epub:trigger elements is supported. If this text is rendered, the audio element is not supported and the test fails"'))
+}).then(function (resp) {
+    var hits = resp.hits.hits;
     console.log(hits);
+}, function (err) {
+    console.trace(err.message);
 });
+
+//client.suggest({
+//    index: 'readium',
+//    body: ejs.TermSuggester('mysuggester')
+//        .text('epub')
+//        .field('body')
+//}, function (error, response) {
+//    var hits = response.hits;
+//    console.log(hits);
+//});
